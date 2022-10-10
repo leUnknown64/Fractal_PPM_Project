c7b5ff8f2d8b88aacf4d0965f89c5d810bb9f0d7
leUnknown64 <dergermankind67@gmail.com>
2022-10-10 14:31:34 -0400

    initial setup. added Pixel struct

---
+++ Image.h
@@ -0,0 +1,11 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+// Pixel Struct represents a pixel in the image. Can be typedefed as pixel_t.
+typedef struct Pixel{
+    unsigned short red;
+    unsigned short green;
+    unsigned short blue;
+} pixel_t;
+
+#endif
