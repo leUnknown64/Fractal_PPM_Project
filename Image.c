d439b55343609733bea19411c544e844599358da
leUnknown64 <dergermankind67@gmail.com>
2022-10-10 22:52:31 -0400

    starter code

--- Image.c
+++ Image.c
@@ -1,1 +1,9 @@
 #include "Image.h"
+
+void writeHeader(FILE* output, unsigned int width, unsigned int height){
+    fprintf(output, "P3\n%d %d\n255\n", width, height);
+}
+
+void writePixels(FILE* output, unsigned int width, unsigned int height){
+    
+}
