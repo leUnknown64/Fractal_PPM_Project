52c6849569c235148c68b1887cb2b4ccc5b3e292
leUnknown64 <dergermankind67@gmail.com>
2022-12-20 21:37:13 +0100

    rename

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "Image.h"
+#include<assert.h>
+#define WIDTH 800
+#define HEIGHT 600
+
+int main(int argc, char* argv[]){
+    // Create output PPM file
+    FILE* PPMOut = fopen("Fractal.ppm", "wb");
+    assert(PPMOut != NULL);
+
+    // Write header data to PPM file
+    writeHeader(PPMOut, WIDTH, HEIGHT);
+
+    // Write pixels to PPM file
+    writePixels(PPMOut, WIDTH, HEIGHT);
+
+    return 0;
+}
